c44328ffb8b90bc07bd3a94f823a39b443828a55
annoinspace <aneesahalmask@gmail.com>
2022-11-22 14:07:07 +0000

    nav and footer added

---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,11 @@
+import { Navbar, Container } from "react-bootstrap"
+
+const MyFooter = () => (
+  <Navbar fixed="bottom" expand="lg" bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#"></Navbar.Brand>
+    </Container>
+  </Navbar>
+)
+
+export default MyFooter
